87b5a45f1cd31048113e8feb9b2b44349c0eb5a3
thachthaiminh <gukucfvn@gmail.com>
2023-10-15 14:10:45 +0700

    Add Header.

--- Learn-TailwindCSS-Coffee-Style/src/App.jsx
+++ Learn-TailwindCSS-Coffee-Style/src/App.jsx
@@ -2,37 +2,57 @@ function App() {
   return (
     <div>
       <div id="root">
-        <div className="content-wrapper max-w-screen-xl text-base mx-auto px-8 bg-slate-400">
-          <header className="">
-            <nav className="">
-              <div className="logo">CoffeeStyle.</div>
-              <ul className="">
-                <li className="">
-                  <a href="#">Home</a>
+        <div className="content-wrapper max-w-screen-xl text-base mx-auto px-8 bg-slate-200">
+          <header className="py-6 mx-10">
+            <nav className="flex flex-row justify-between items-center">
+              <div className="logo basis-2/6 text-center text-xl font-semibold cursor-pointer">
+                CoffeeStyle.
+              </div>
+              <ul className="basis-3/6 flex items-center justify-end gap-8 uppercase text-sm text-gray-500 font-medium ">
+                <li className="ttm-top-menu-item">
+                  <a href="Home">Home</a>
                 </li>
-                <li className="">
-                  <a href="#">Our Product</a>
+                <li className="ttm-top-menu-item">
+                  <a href="Product">Product</a>
                 </li>
-                <li className="">
-                  <a href="#">Blog</a>
+                <li className="ttm-top-menu-item">
+                  <a href="Blog">Blog</a>
                 </li>
-                <li className="">
-                  <a href="#">About</a>
+                <li className="ttm-top-menu-item">
+                  <a href="About">About</a>
                 </li>
-                <li className="">
-                  <a href="#">Contact</a>
+                <li className="ttm-top-menu-item">
+                  <a href="Contact">Contact</a>
                 </li>
-                <li className="">
-                  <a href="#">StyleGuide</a>
+                <li className="ttm-top-menu-item">
+                  <a href="StyleGuide">StyleGuide</a>
                 </li>
-                <li className="">
-                  <a href="#">thachthaiminh</a>
+                <li className="ttm-top-menu-item">
+                  <a href="thachthaiminh">thachthaiminh</a>
                 </li>
               </ul>
-              <ul className="">
-                <li className="">
-                  <a href="" className="">
-                    Card
+              <ul className="basis-1/6 flex justify-start items-center ml-16 uppercase text-sm text-gray-500 font-medium">
+                <li className="ttm-top-menu-item">
+                  <a href="" className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="ttm-icon"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                      />
+                    </svg>
+
+                    <span className="mx-2">Cart</span>
+                    <span className="ttm-badge-circle bg-orange-400 text-white">
+                      10
+                    </span>
                   </a>
                 </li>
               </ul>
--- Learn-TailwindCSS-Coffee-Style/tailwind.config.js
+++ Learn-TailwindCSS-Coffee-Style/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-coffee": "#c89f94",
+      },
+    },
   },
   plugins: [],
 };
